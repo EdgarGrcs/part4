c85f053e1efce9d1008252acdf7f7aae18347830
EdgarGrcs <edgar.gricius2@gmail.com>
2023-09-04 19:40:33 +0200

    finish exercises 4.4 and 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,10 +6,39 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     
+  let result = 0;
 
-    return 
-}  
+    blogs.forEach((array) => {
+      result = result + array.likes;
+      console.log(result);
+    })  
+
+    return result
+}
+
+const favoriteBlog = (blogs) => {
+
+  let result = {
+    title: blogs[0].title,
+    author:blogs[0].author,
+    likes: blogs[0].likes
+  }
+
+  for (let i = 0; i < blogs.length -1; i++){
+    if (blogs[i].likes > result.likes){
+      result.title = blogs[i].title;
+      result.author = blogs[i].author;
+      result.likes = blogs[i].likes; 
+    }
+  }
+
+  console.log(result);
+
+  return result;
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
