4cd86deae2c5284ccfd8a007de75014bab1f2f45
EdgarGrcs <edgar.gricius2@gmail.com>
2023-09-30 21:07:58 +0200

    finish 4.8

---
+++ tests/blog_api.test.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+//const Blog = require("../models/bloglist");
+const api = supertest(app);
+
+
+
+test('returns correct amount of blogposts', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(1);
+});
