85541c5eecfb1b6e5d600193314e49795f90bc2f
EdgarGrcs <edgar.gricius2@gmail.com>
2023-10-01 14:38:02 +0200

    finish 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,11 +3,25 @@ const supertest = require("supertest");
 const app = require("../app");
 //const Blog = require("../models/bloglist");
 const api = supertest(app);
+const helper = require("./test_helper");
+const Blog = require("../models/bloglist");
 
-test("returns correct amount of blogposts", async () => {
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+    const promiseArray = blogObjects.map(blog => blog.save());
+    await Promise.all(promiseArray);
+})
+
+
+test("returns correct amount of blogposts - 4.8", async () => {
   const response = await api.get("/api/blogs");
-
-  expect(response.body).toHaveLength(1);
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
-test("")
+test("unique identifier property is named id - 4.9", async () => {
+    const blog = await Blog.find({});
+    expect(blog[0]._id).toBeDefined();
+})
---
+++ tests/test_helper.js
@@ -0,0 +1,15 @@
+const Blog = require("../models/bloglist");
+
+
+const initialBlogs = [
+    {
+        title:"Wolf of wallstreet",
+        author:"Jordan Belfort",
+        url:"makemoneyfast.com",
+        likes:"69"
+    }
+]
+
+
+
+module.exports = {initialBlogs}
